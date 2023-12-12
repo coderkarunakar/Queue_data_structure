c56f96953a53c3cb9e4602a4968f20202d84f1fd
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2023-12-12 08:18:13 +0530

    Intro to Queue DS

---
+++ intro.py
@@ -0,0 +1,15 @@
+# Intro to Queue Data Structures
+#as we know stack follow LIFO last in first out
+#but this Queue follows FIFO first in first out
+#it had this 5 functionality
+#1.Enqueue(i.e push in stack)
+#2.Dequeue(i.e pop in stack)
+#3.size()
+#4.isEmpty()
+#5.front()
+
+
+#Queue has 2 parts i.e front(starting point(First element in Queue)) and rear(end point(last element in the Queue)) in a Queue
+
+
+#Please look at notes as well
